4d90b3b60fdeaaf5f96afe178d33eac8537dfa43
jordanhudgens <jordan@devcamp.com>
2018-06-09 17:32:12 -0600

    Built out the registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+  include AuthTokenConcern
+
+  def create
+    user = User.create!(
+      email: params['user']['email'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation'],
+      auth_token: unique_auth_token
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: { status: :created }
+    else
+      render json: { status: 500 }
+    end
+  end
+end
