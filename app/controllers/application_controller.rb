6dbc2850a9c7c5f77a3c523ac471ef8593f37774
jordanhudgens <jordan@devcamp.com>
2018-06-09 19:16:38 -0600

    Added current user method with debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def current_user
+    puts "X" * 500, session[:user_id]
+    if user_id = session[:user_id]
+      @current_user ||= User.find(user_id)
+    else
+      nil
+    end
+  end
 end
