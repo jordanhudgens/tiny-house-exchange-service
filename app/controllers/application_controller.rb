7adbc05626138165e19ad3406a7587af46f93715
jordanhudgens <jordan@devcamp.com>
2018-06-12 08:10:06 -0600

    Added debugging output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :current_user
+
   def current_user
+    puts "Y" * 500, session[:user_id]
     if user_id = session[:user_id]
       @current_user ||= User.find(user_id)
     else
