abb2bb601f885c675840f142faaa59487c32ef6a
jordanhudgens <jordan@devcamp.com>
2018-07-11 23:06:30 -0600

    added a featured image virtual attribute

--- app/models/home.rb
+++ app/models/home.rb
@@ -2,4 +2,10 @@ class Home < ApplicationRecord
   has_one_attached :featured_image
   has_many_attached :images
   belongs_to :user
+
+  def featured_image_url
+    if self.featured_image.attachment
+      self.featured_image.attachment.service_url
+    end
+  end
 end
