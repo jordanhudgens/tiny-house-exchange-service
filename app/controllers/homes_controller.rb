e3b8565dcf6364fe16e57aa3406533dd37f41554
jordanhudgens <jordan@devcamp.com>
2018-07-11 22:50:29 -0600

    Removed comments from scaffold

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,19 +1,16 @@
 class HomesController < ApplicationController
   before_action :set_home, only: [:show, :update, :destroy]
 
-  # GET /homes
   def index
     @homes = Home.all
 
     render json: @homes
   end
 
-  # GET /homes/1
   def show
     render json: @home
   end
 
-  # POST /homes
   def create
     @home = Home.new(home_params)
 
@@ -24,7 +21,6 @@ class HomesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /homes/1
   def update
     if @home.update(home_params)
       render json: @home
@@ -33,19 +29,20 @@ class HomesController < ApplicationController
     end
   end
 
-  # DELETE /homes/1
   def destroy
     @home.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_home
       @home = Home.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def home_params
-      params.require(:home).permit(:name, :description, :user_id)
+      params.require(:home).permit(
+        :name,
+        :description,
+        :user_id
+      )
     end
 end
