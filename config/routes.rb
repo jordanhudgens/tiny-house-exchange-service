830763d1aa63231d56a4cfe0cb0cffedad9dbb3d
jordanhudgens <jordan@devcamp.com>
2018-11-03 17:56:06 -0500

    Added auth endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :homes
-  post 'register_user' => 'registrations#create'
+
+  resources :registrations, only: [:create]
+  resources :sessions, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
 end
